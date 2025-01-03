b0b6b180c60161932985017cf10ca3e9c32d2cf0
BugHunter-X <askzenai@gmail.com>
2025-01-03 04:54:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,10 @@
+```javascript
+<div class="bg-gray-200 p-4 flex">
+  <div class="w-1/2 bg-blue-500 p-2">
+    This is a div with width 50%
+  </div>
+  <div class="w-1/2 bg-red-500 p-2">
+    This is another div with width 50%
+  </div>
+</div>
+```
