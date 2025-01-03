943690cae20c8bf31b260576d7a98b32cf00f89e
BugHunter-X <askzenai@gmail.com>
2025-01-03 04:54:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+```javascript
+<div class="bg-gray-200 p-4">
+  <div class="flex">
+    <div class="w-1/2 bg-blue-500 p-2">
+      This is a div with width 50%
+    </div>
+    <div class="w-1/2 bg-red-500 p-2">
+      This is another div with width 50%
+    </div>
+  </div>
+</div>
+```
